f24915b8f8694544aa7d41d088e5741b403ae551
Robert Estey <rwe001@perfect.com>
2017-04-05 21:40:49 -0400

    resetting map and going back to fix

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -11,10 +11,29 @@ export class DatabaseService{
 
   constructor(private http:Http) {
 
-    console.log('DatabaseService Initialized');
+    console.log('DatabaseService.constructor');
   }
 
   getResponse(httpAddress:string) {
+    console.log('DatabaseService.getResponse');
     return this.http.get(httpAddress).map(res => {return res}).catch(this.handleError);
+  }
+
+  getMarkers() {
+    console.log('DatabaseService.getMarkers');
+    var markers = "";
+    return markers;
+  }
+
+  addMarker(newMarker) {
+    console.log('DatabaseService.addMarker');
+  }
+
+  updateMarker(marker, newLat, newLng) {
+    console.log('DatabaseService.updateMarker');
+  }
+
+  removeMarker(marker) {
+    console.log('DatabaseService.removeMarker');
   }
 }
